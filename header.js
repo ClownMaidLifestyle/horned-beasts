eec265aef8d2b5880c4d3a992c7c79eba49f5fad
Clownmaidlifestyle <e.edson@protonmail.ch>
2023-07-18 14:06:39 +0100

    migrating to other accoutn

---
+++ header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <h1>Horned Beasties of the world</h1>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Header from './header.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header />
     </div>
   );
 }
