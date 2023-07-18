c4412911d93aa89187f379df83d99be67b22e6f1
Elisa Edson <e.edson@protonmail.ch>
2023-07-18 22:25:55 +0100

    favourite function iterates correctly

--- src/main.js
+++ src/main.js
@@ -9,12 +9,25 @@ export default function Main() {
 
     let [favourites, setFavourites] = useState(0);
     function handleFavourites(){
-        setFavourites(favourites++);
+        setFavourites(favourites + 1);
+        console.log(favourites);
+        console.log("favourites function running!!!!!!!!!!");
     }
   return (
     <main>
-        <Beast title="The Rhino Beetle" imageUrl={Beetle} description="These insects use their horns to fight rivals and display their prowess to mates" handleFavourites={handleFavourites} favourites={favourites}/>
-        <Beast title="The Elk" imageUrl={Elf} description="The majestic stag's antlers are a kind of horn that can grow up to 9kg in weight each" handleFavourites={handleFavourites} favourites={favourites}/> 
+        <Beast 
+        title="The Rhino Beetle" 
+        imageUrl={Beetle} 
+        description="These insects use their horns to fight rivals and display their prowess to mates" 
+        handleFavourites={handleFavourites} 
+        favourites={favourites}/>
+
+        <Beast 
+        title="The Elk" 
+        imageUrl={Elf} 
+        description="The majestic stag's antlers are a kind of horn that can grow up to 9kg in weight each"
+         handleFavourites={handleFavourites} 
+         favourites={favourites}/> 
     </main>
   )
 }
