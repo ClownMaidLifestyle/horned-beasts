a23458926c4e0c15c92eb2658f99864de96a7951
Elisa Edson <e.edson@protonmail.ch>
2023-07-18 15:32:35 +0100

    footer component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './header.js'
 import Main from './main.js' 
+import Footer from './footer.js'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
