dd3038391b15599978359786d337d51366cc5aa2
Elisa Edson <e.edson@protonmail.ch>
2023-07-18 16:39:52 +0100

    working images

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './header.js'
 import Main from './main.js' 
 import Footer from './footer.js'
 import Beast from './beast.js'
+import Beetle from './images/Beetle.jpg'
+import Elf from './images/Elk.jpg'
 
 function App() {
   return (
@@ -10,8 +12,8 @@ function App() {
       <Header />
       <Main />
       <Footer />
-      <Beast title="The Rhino Beetle" imageUrl="images/Beetle.jpg" description="These insects use their horns to fight rivals and display their prowess to mates"/>
-      <Beast title="The Elk" imageUrl="images/Elk.jpg" description="The majestic stag's antlers are a kind of horn that cna grow up to 9kg in weight each"/> 
+      <Beast title="The Rhino Beetle" imageUrl={Beetle} description="These insects use their horns to fight rivals and display their prowess to mates"/>
+      <Beast title="The Elk" imageUrl={Elf} description="The majestic stag's antlers are a kind of horn that can grow up to 9kg in weight each"/> 
     </div>
   );
 }
