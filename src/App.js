965abe6c5c7b8aad22c6e9044f4c813258402fab
Elisa Edson <e.edson@protonmail.ch>
2023-07-18 16:27:45 +0100

    beast components linked, images broken

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './header.js'
 import Main from './main.js' 
 import Footer from './footer.js'
+import Beast from './beast.js'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Header />
       <Main />
       <Footer />
+      <Beast title="The Rhino Beetle" imageUrl="images/Beetle.jpg" description="These insects use their horns to fight rivals and display their prowess to mates"/>
+      <Beast title="The Elk" imageUrl="images/Elk.jpg" description="The majestic stag's antlers are a kind of horn that cna grow up to 9kg in weight each"/> 
     </div>
   );
 }
--- src/beast.js
+++ src/beast.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function beast(title,imageUrl,description) {
+export default function beast({title,imageUrl,description}) {
   return (
     <div>
         <h2>{title}</h2>
-        <img src={imageUrl} alt ="a horned beast!"/>
+        <img src={imageUrl} alt="a horned beast!"/>
         <span>{description}</span>
     </div>
   )
