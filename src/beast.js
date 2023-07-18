a4f65309cc524cb9ab3c5bf386733b340079ecfd
Elisa Edson <e.edson@protonmail.ch>
2023-07-18 16:00:46 +0100

    beast component

---
+++ src/beast.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function beast(title,imageUrl,description) {
+  return (
+    <div>
+        <h2>{title}</h2>
+        <img src={imageUrl} alt ="a horned beast!"/>
+        <span>{description}</span>
+    </div>
+  )
+}
