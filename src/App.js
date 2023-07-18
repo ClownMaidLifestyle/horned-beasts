af870c71e3fbda9869b65da2d4501e223e50c574
Elisa Edson <e.edson@protonmail.ch>
2023-07-18 16:45:42 +0100

    moved beast components into main.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import './App.css';
 import Header from './header.js'
 import Main from './main.js' 
 import Footer from './footer.js'
-import Beast from './beast.js'
-import Beetle from './images/Beetle.jpg'
-import Elf from './images/Elk.jpg'
 
 function App() {
   return (
@@ -12,8 +9,6 @@ function App() {
       <Header />
       <Main />
       <Footer />
-      <Beast title="The Rhino Beetle" imageUrl={Beetle} description="These insects use their horns to fight rivals and display their prowess to mates"/>
-      <Beast title="The Elk" imageUrl={Elf} description="The majestic stag's antlers are a kind of horn that can grow up to 9kg in weight each"/> 
     </div>
   );
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Beast from './beast.js'
+import Beetle from './images/Beetle.jpg'
+import Elf from './images/Elk.jpg'
 
 export default function main() {
   return (
-    <main>main</main>
+    <main>
+        <Beast title="The Rhino Beetle" imageUrl={Beetle} description="These insects use their horns to fight rivals and display their prowess to mates"/>
+        <Beast title="The Elk" imageUrl={Elf} description="The majestic stag's antlers are a kind of horn that can grow up to 9kg in weight each"/> 
+    </main>
   )
 }
