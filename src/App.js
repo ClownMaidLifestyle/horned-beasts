032fca1956bacc74ad2e12fdd22528191c9a92e9
Elisa Edson <e.edson@protonmail.ch>
2023-07-18 15:28:55 +0100

    main component connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './header.js'
+import Main from './main.js' 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
