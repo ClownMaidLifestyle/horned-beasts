f1c5099c17a1011e34eaf366229c7c8acdaedc52
Elisa Edson <e.edson@protonmail.ch>
2023-07-19 22:29:10 +0100

    im so tired

---
+++ src/modals/imagepopup.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ImagePopup(imageSrc) {
+    const imageSrcImsotired = imageSrc[imageSrc]
+  return (
+    <div>
+        <img className="popUpImage" src={imageSrcImsotired} alt="a Horned beast!!!"/>
+    </div>
+  )
+}
