39c887b646b1bee856415eebfbe6ffb9e36fd2a6
Elisa Edson <e.edson@protonmail.ch>
2023-07-19 13:53:21 +0100

    favourites function now works indepdendently for each image

--- src/beast.js
+++ src/beast.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
-export default function beast({title,imageUrl,description, handleFavourites, favourites, beastNumber}) {
+
+
+export default function Beast({title,imageUrl,description, beastNumber}) {
+  let [favourites, setFavourites] = useState([0,0]);
+
+
+  function HandleFavourites(imageIndex){
+    console.log(imageIndex);
+    console.log(favourites);
+    setFavourites(favourites.map((favourite) => favourite + 1));
+    console.log("favourites function running!!!!!!!!!!");
+  }
   return (
     <div>
         <h2>{title}</h2>
-        <img onClick={() => handleFavourites(beastNumber)} src={imageUrl} alt="a horned beast!"/>
+        <div>
+            <img className="hornImage" onClick={() =>HandleFavourites(beastNumber)} src={imageUrl} alt="a horned beast!"/>
+        </div>
         <span>{description} {beastNumber}</span>
         <div>❤Favourites❤ = {favourites[beastNumber]}</div>
     </div>
--- src/main.js
+++ src/main.js
@@ -3,33 +3,20 @@ import Beast from './beast.js';
 import Beetle from './images/Beetle.jpg';
 import Elf from './images/Elk.jpg';
 
-import { useState } from 'react';
 
-export default function Main() {
-
-    let [favourites, setFavourites] = useState([0,0]);
-    function handleFavourites(imageIndex){
-        console.log(imageIndex);
-        console.log(favourites);
-        setFavourites(favourites.map((favourite) => favourite + 1));
-        console.log("favourites function running!!!!!!!!!!");
-    }
+export default function Main(){
   return (
     <main>
         <Beast 
         title="The Rhino Beetle" 
         imageUrl={Beetle} 
         description="These insects use their horns to fight rivals and display their prowess to mates" 
-        handleFavourites={handleFavourites} 
-        favourites={favourites}
         beastNumber={0}/>
 
         <Beast 
         title="The Elk" 
         imageUrl={Elf} 
         description="The majestic stag's antlers are a kind of horn that can grow up to 9kg in weight each"
-         handleFavourites={handleFavourites} 
-         favourites={favourites}
          beastNumber={1}/> 
     </main>
   )
