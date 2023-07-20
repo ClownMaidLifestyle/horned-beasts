59b323595441970b730a54142233c830d03aa465
Elisa Edson <e.edson@protonmail.ch>
2023-07-20 10:54:53 +0100

    images display properly

--- src/beast.js
+++ src/beast.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { useState } from 'react';
 
+import ImagePopup from './modals/imagepopup.js';
 
 
-export default function Beast({title,imageUrl,description, beastNumber}) {
-  let [favourites, setFavourites] = useState([0,0]);
+export default function Beast({title,imageUrl,description, beastId}) {
+  let [favourites, setFavourites] = useState(0);
 
 
   function HandleFavourites(imageIndex){
     console.log(imageIndex);
     console.log(favourites);
-    setFavourites(favourites.map((favourite) => favourite + 1));
+    setFavourites(favourites => favourites + 1);
     console.log("favourites function running!!!!!!!!!!");
   }
   return (
     <div>
         <h2>{title}</h2>
         <div>
-            <img className="hornImage" onClick={() =>HandleFavourites(beastNumber)} src={imageUrl} alt="a horned beast!"/>
+            <img className="hornImage" onClick={() => HandleFavourites()} src={imageUrl} alt="a horned beast!"/>
+            {console.log(imageUrl)}
         </div>
-        <span>{description} {beastNumber}</span>
-        <div>❤Favourites❤ = {favourites[beastNumber]}</div>
+        <span>{description}</span>
+        <div>❤Favourites❤ = {favourites}</div>
+
+        <ImagePopup imageSrc={beastId}/>
     </div>
   )
 }
--- src/modals/imagepopup.js
+++ src/modals/imagepopup.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 export default function ImagePopup(imageSrc) {
-    const imageSrcImsotired = imageSrc[imageSrc]
+    const imageSrcImsotired = imageSrc
+    console.log(imageSrc["imageSrc"])
+    console.log("i'm so tired + " +imageSrcImsotired);
   return (
     <div>
-        <img className="popUpImage" src={imageSrcImsotired} alt="a Horned beast!!!"/>
+        <img className="popUpImage" src={`${imageSrc["imageSrc"]}`} alt="a Horned beast!!!"/>
     </div>
   )
 }
