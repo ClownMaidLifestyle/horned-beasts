3229e3d1a2020bde3837956f34fd247fbedf29fa
Elisa Edson <e.edson@protonmail.ch>
2023-07-20 11:47:35 +0100

    modal shows up

--- src/beast.js
+++ src/beast.js
@@ -6,7 +6,17 @@ import ImagePopup from './modals/imagepopup.js';
 
 export default function Beast({title,imageUrl,description, beastId}) {
   let [favourites, setFavourites] = useState(0);
+  let [showModal, setShowModal] = useState(false);
 
+  function openModal() {
+    setShowModal(true);
+    console.log(showModal);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+    console.log(showModal);
+  }
 
   function HandleFavourites(imageIndex){
     console.log(imageIndex);
@@ -18,13 +28,13 @@ export default function Beast({title,imageUrl,description, beastId}) {
     <div>
         <h2>{title}</h2>
         <div>
-            <img className="hornImage" onClick={() => HandleFavourites()} src={imageUrl} alt="a horned beast!"/>
+            <img className="hornImage" onClick={() => {HandleFavourites(); openModal() }} src={imageUrl} alt="a horned beast!" />
             {console.log(imageUrl)}
         </div>
         <span>{description}</span>
         <div>❤Favourites❤ = {favourites}</div>
 
-        <ImagePopup imageSrc={beastId}/>
+        {showModal && <ImagePopup imageSrc={beastId} closeModal={closeModal}/>}
     </div>
   )
 }
--- src/modals/imagepopup.js
+++ src/modals/imagepopup.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function ImagePopup(imageSrc) {
+export default function ImagePopup(imageSrc, closeModal) {
     const imageSrcImsotired = imageSrc
     console.log(imageSrc["imageSrc"])
     console.log("i'm so tired + " +imageSrcImsotired);
   return (
     <div>
-        <img className="popUpImage" src={`${imageSrc["imageSrc"]}`} alt="a Horned beast!!!"/>
+        <img className="popUpImage" src={`${imageSrc["imageSrc"]}`} alt="a Horned beast!!!" onclick={closeModal}/>
     </div>
   )
 }
